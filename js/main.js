07c50917ca2ef9663709a1291747b4c3fcaf1503
rubacodes <rubacodes@gmail.com>
2022-05-23 15:00:12 +0200

    impostato app vue

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+      {
+        text: 'Andare dal veterinario',
+        completed: false,
+      },
+      {
+        text: 'Fare la spesa',
+        completed: false,
+      },
+      {
+        text: 'Pulire la camera',
+        completed: false,
+      },
+    ],
+  },
+  methods: {},
+});
