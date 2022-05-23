cd8439c33d0a47180be2091fcbf717e326a361e8
rubacodes <rubacodes@gmail.com>
2022-05-23 20:18:35 +0200

    edit con doppio click del todo

--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@ const app = new Vue({
   el: '#app',
   data: {
     userInput: '',
+    userEdit: '',
     todos: [
       {
         text: 'Andare dal veterinario',
         completed: true,
+        processing: false,
       },
       {
         text: 'Fare la spesa',
         completed: false,
+        processing: false,
       },
       {
         text: 'Pulire la camera',
         completed: false,
+        processing: false,
       },
     ],
   },
@@ -23,12 +27,19 @@ const app = new Vue({
         this.todos.push({
           text: this.userInput,
           completed: false,
+          processing: false,
         });
       }
       this.userInput = '';
     },
     removeTodo(index) {
       this.todos.splice(index, 1);
+    },
+    edit(index) {
+      console.log(index);
+      this.todos[index].text = this.userEdit;
+      this.todos[index].processing = !this.todos[index].processing;
+      this.userEdit = '';
     },
   },
 });
